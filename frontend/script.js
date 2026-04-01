b12d293fc3f66573e965a801435f8ea87f17b723
abhinav <iamabhinav2244@gmail.com>
2026-04-02 03:11:00 +0530

    added join to js

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,7 @@
-const btn = document.getElementById("createBtn");
+// CREATE ROOM
+const createBtn = document.getElementById("createBtn");
 
-btn.addEventListener("click", () => {
+createBtn.addEventListener("click", () => {
     const username = document.getElementById("username").value;
 
     if (!username) {
@@ -17,10 +18,51 @@ btn.addEventListener("click", () => {
     })
     .then(res => res.json())
     .then(data => {
-        console.log("Response:", data);
-        document.getElementById("roomDisplay").innerText = "Player: "+data.username +" Room ID: " + data.roomId;
+        showGameScreen(data);
     })
-    .catch(err => {
-        console.error("Error:", err);
-    });
+    .catch(err => console.error("Error:", err));
 });
+
+
+// JOIN ROOM
+const joinBtn = document.getElementById("joinBtn");
+
+joinBtn.addEventListener("click", () => {
+    const username = document.getElementById("joinUsername").value;
+    const roomId = document.getElementById("joinRoomId").value;
+
+    if (!username || !roomId) {
+        alert("Enter username and room ID");
+        return;
+    }
+
+    fetch(`http://localhost:8080/game/join/${roomId}`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ username: username })
+    })
+    .then(res => res.json())
+    .then(data => {
+        showGameScreen(data);
+    })
+    .catch(err => console.error("Error:", err));
+});
+
+
+function showGameScreen(data) {
+    console.log("Response:", data);
+
+    // switch UI
+    document.getElementById("createScreen").style.display = "none";
+    document.getElementById("joinScreen").style.display = "none";
+    document.getElementById("gameScreen").style.display = "block";
+
+    // show data
+    document.getElementById("roomId").innerText =
+        "Room ID: " + data.roomId;
+
+    document.getElementById("playerName").innerText =
+        "Player: " + data.username;
+}
